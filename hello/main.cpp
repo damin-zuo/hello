13fd5f9954966a2654aef61cf7cc72df43616760
Zuo <szuo@ukaachen.de>
2018-03-05 15:15:19 +0100

    Initial commit

---
+++ hello/main.cpp
@@ -0,0 +1,45 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct  Node
+{
+	char ch[128];
+
+	Node* next;
+};
+Node lst = { 0 }; // head1
+int length = 0;
+
+
+void  fixed_allloc()
+{
+	Node* Mem = (Node*)malloc(sizeof(Node));
+	Mem->next = lst.next;
+	lst.next = Mem;
+}
+
+void fixed_free()
+{
+	Node* p = lst.next;
+	while (p)
+	{
+
+		lst.next = p->next;
+		free(p);
+		p = lst.next;
+		length -= 1;
+	}
+}
+
+int main()
+{
+	lst.next = NULL;
+	for (int i = 0; i < 10; i++)
+	{
+		fixed_allloc();
+		length += 1;
+	}	
+	fixed_free();
+	return 0;
+}
---
+++ hello/number.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main() {
+	unsigned short year;
+	unsigned char month;
+	unsigned char day;
+
+	int y, m, d;
+	printf("please input the date:");
+	scanf("%d,%d,%d", &y, &m, &d);
+
+	year = (unsigned short) y;
+	month = (unsigned char) m;
+	day = (unsigned char) d;
+
+	printf("%d, %d, %d",  year,  month,  day);
+	return 0;
+}
